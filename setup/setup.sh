71631fe55c499e18f2009e80bc4cb9547d307148
Mudit Porwal <mudit.porwal@gmail.com>
2016-10-11 13:32:59 +0800

    Updated Setup Script

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,6 +1,36 @@
 #!/bin/bash
-echo "Installing Packages : Need Root Permission"
-for x in $(cat ./basic.lst); do pacman -S --noconfirm $x; done
-for x in $(cat ./prog.lst); do pacman -S --noconfirm $x; done
 
-chsh -s /bin/zsh
+echo "\nUpdating System - PAC Repositories";
+sleep 0.5;
+sudo pacman -Syu;
+
+echo "\nUpdating System - AUR Repositories";
+sleep 0.5;
+yaourt -Syu --aur;
+
+
+_update="yaourt -Syua --noconfirm";
+_query="yaourt -Q"
+
+for x in $(cat ./basic.lst); 
+	do 
+		if ! `$_query $x` 2>/dev/null 1>&2; then
+ 		echo "\n\nPackage $x not installed, installing";
+		sleep 0.5; 		
+		$_update $x
+   		#exit $?;
+		fi
+	done
+for x in $(cat ./prog.lst); 
+	do 
+		if ! `$_query $x` 2>/dev/null 1>&2; then
+ 		echo "\n\nPackage $x not installed, installing";
+		sleep 0.5; 		
+		$_update $x
+   		#exit $?;
+		fi
+	done
+echo "\n\nChange Default Shell to Zsh";
+sleep 0.5; 
+chsh -s /usr/bin/zsh;
+echo "Set Default Java using -- archlinux-java status ; archlinux-java set <JAVA_ENV_NAME>"
