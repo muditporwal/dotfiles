0647bd9efa6a96e6eccd85e2810759f6dd81fb56
Mudit Porwal <mudit.porwal@gmail.com>
2016-10-15 05:38:13 +0800

    Updated Setup Script to working

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-echo "\nUpdating System - PAC Repositories";
+echo -e "\nUpdating System - PAC Repositories";
 sleep 0.5;
 sudo pacman -Syu;
 
-echo "\nUpdating System - AUR Repositories";
+echo -e "\nUpdating System - AUR Repositories";
 sleep 0.5;
 yaourt -Syu --aur;
 
@@ -12,25 +12,26 @@ yaourt -Syu --aur;
 _update="yaourt -Syua --noconfirm";
 _query="yaourt -Q"
 
-for x in $(cat ./basic.lst); 
-	do 
-		if ! `$_query $x` 2>/dev/null 1>&2; then
- 		echo "\n\nPackage $x not installed, installing";
-		sleep 0.5; 		
-		$_update $x
-   		#exit $?;
+for x in $(cat ./basic.lst) $(cat ./prog.lst) ; 
+	do	
+		if [ -z `$_query $x` ] 2>/dev/null 1>&2; then
+	 		echo -e "\n\nPackage $x not installed, installing";
+		        existingPackage=`$_query | grep $x`;	
+			if [ -n $existingPackage ] 2>/dev/null 1>&2; then
+				sleep 0.5; 		
+				$_update $x;
+			else
+				echo -e "\n\nSimilar Package Installed ::: $existingPackage";
+			fi
+#  		#exit $?;
 		fi
 	done
-for x in $(cat ./prog.lst); 
-	do 
-		if ! `$_query $x` 2>/dev/null 1>&2; then
- 		echo "\n\nPackage $x not installed, installing";
-		sleep 0.5; 		
-		$_update $x
-   		#exit $?;
-		fi
-	done
-echo "\n\nChange Default Shell to Zsh";
+
+
+
+if [ $SHELL != '/usr/bin/zsh' ]; then
+echo -e "\n\nChange Default Shell to Zsh";
 sleep 0.5; 
 chsh -s /usr/bin/zsh;
-echo "Set Default Java using -- archlinux-java status ; archlinux-java set <JAVA_ENV_NAME>"
+fi
+echo -e "Set Default Java using -- archlinux-java status ; archlinux-java set <JAVA_ENV_NAME>"
