83dc983beeb231f60661964e501983482ad46089
Mudit Porwal <muditporwal@users.noreply.github.com>
2023-09-27 11:01:20 +0800

    Added Configuration file for podman

--- setup/tools/podman-setup.sh
+++ setup/tools/podman-setup.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
 # Define the registries you want to add
-registries=("docker.io" "quay.io")
+registries=("docker.io" "quay.io" "example.com")
 
 # Path to the registries.conf file
 registries_conf="/etc/containers/registries.conf"
 
 # Check if the file exists
 if [ -f "$registries_conf" ]; then
+  # Check if the registry entry already exists
+  if ! grep -q "registries =" "$registries_conf"; then
+    echo -e "\n[registries.search]\nregistries = [" | sudo tee -a "$registries_conf"
+  fi
+
   # Add registries to registries.conf
   for registry in "${registries[@]}"; do
-    grep -q "$registry" "$registries_conf" || echo "registries = append(source_dict(), '$registry')" | sudo tee -a "$registries_conf"
+    grep -q "\"$registry\"" "$registries_conf" || echo "  \"$registry\"," | sudo tee -a "$registries_conf"
   done
 
   echo "Podman configuration has been updated."
@@ -22,6 +27,3 @@ else
   echo "Error: $registries_conf not found. Please make sure Podman is installed."
   exit 1
 fi
-
-
-
