73bcab9f80250093f74370c6eb02ab7640bf1945
Mudit Porwal <muditporwal@users.noreply.github.com>
2023-09-27 09:59:34 +0800

    Added Command for Fastest Mirror

--- run_once_040_update_system.sh
+++ run_once_040_update_system.sh
@@ -3,9 +3,12 @@
 # Getting to the context directory
 cd ~/.local/share/chezmoi/setup
 
+echo "Finding Fastest Mirror for Singapore(Please update command if change in location)"
+sudo pacman-mirrors --fasttrack
+sudo reflector --country 'Singapore' --latest 10 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
+sudo pacman -Syy
+echo "Completed Finding Fastest Mirror"
 
-# echo "-----------------------------------------------------------------------------"
 echo "Updating packages before install"
 sudo pacman -Syyu 1>/dev/null 2>&1
 echo "Completed updating packages before install"
-echo "-----------------------------------------------------------------------------"
