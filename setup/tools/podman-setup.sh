cd1bd11529053b33f0a48e56c97b26bb1bfcc927
Mudit Porwal <muditporwal@users.noreply.github.com>
2023-09-27 14:57:19 +0000

    Updated the script to have comma seperated registries

--- setup/tools/podman-setup.sh
+++ setup/tools/podman-setup.sh
@@ -15,7 +15,9 @@ if [ -f "$registries_conf" ]; then
   sleep 5;
  else
   # Create the registries.conf file with the [registries.search] section and the registries entry
-  combined_registries="registries: [$(printf " '%s' " "${registries[@]}")]"
+  combined_registries=$(printf " '%s' ," "${registries[@]}")
+  combined_registries="${combined_registries%,}]"
+  combined_registries="registries: [$combined_registries]"
   echo -e "[registries.search]\n$combined_registries" | sudo tee -a "$registries_conf"
   echo "Podman configuration has been created and updated."
   # Refresh the Podman image cache
