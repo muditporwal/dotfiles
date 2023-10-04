1b93cab7e2b1811af19c914aa8ec9b9e41f64a7d
Mudit Porwal <mudit.porwal@users.noreply.github.com>
2023-10-04 13:05:05 +0800

    Setting Up Speech dispatcher

--- setup/tools/festival-setup.sh
+++ setup/tools/festival-setup.sh
@@ -1,1 +1,38 @@
 #!/bin/bash
+
+# Function to run commands with sudo
+run_with_sudo() {
+  command="$1"
+  echo "Running: $command"
+  sudo sh -c "$command"
+  if [ $? -ne 0 ]; then
+    echo "Error: Failed to run command with sudo."
+    exit 1
+  fi
+}
+
+# Create a systemd service unit for Festival TTS
+run_with_sudo "cat <<EOL > /etc/systemd/system/festival.service
+[Unit]
+Description=Festival TTS Server
+
+[Service]
+ExecStart=/usr/bin/festival --server
+
+[Install]
+WantedBy=multi-user.target
+EOL"
+
+# Reload systemd
+run_with_sudo "systemctl daemon-reload"
+
+# Enable the festival service at startup
+run_with_sudo "systemctl enable festival.service"
+
+# Start the festival service
+run_with_sudo "systemctl start festival.service"
+
+# Check the status of the festival service
+run_with_sudo "systemctl status festival.service"
+
+echo "Festival TTS server setup completed."
--- setup/tools/speech-dispatcher-setup.sh
+++ setup/tools/speech-dispatcher-setup.sh
@@ -1,5 +1,33 @@
 #!/bin/bash
+
+echo "This speech dispatcher configuration depends on FESTIVAL"
+sleep 10
+
+# Function to create an override.conf file
+create_override_conf() {
+  conf_file="/etc/systemd/system/speech-dispatcherd.service.d/override.conf"
+  echo "Creating $conf_file..."
+  cat <<EOL | sudo tee "$conf_file" >/dev/null
+[Unit]
+After=festival.service
+EOL
+  if [ $? -ne 0 ]; then
+    echo "Error: Failed to create $conf_file."
+    exit 1
+  fi
+}
+
+# Create the override.conf file
+create_override_conf
+
+echo "Override configuration for speech-dispatcherd.service created."
+
+sudo systemctl daemon-reload
+
+sudo systemctl restart festival speech-dispatcherd
+
+#spd-conf
+#Linking user configuration instead to the git repo
 current_dir=$(pwd)
-# mkdir -p "$HOME/.config/variety"
-# rm -f "$HOME/.config/variety/variety.conf"
-# ln -sf "$current_dir/tools/conf/variety.conf" "$HOME/.config/variety/variety.conf"
+mkdir -p "$HOME/.config/speech-dispatcher"
+ln -sf "$current_dir/tools/conf/speechd.conf" "$HOME/.config/speech-dispatcher/speechd.conf"
