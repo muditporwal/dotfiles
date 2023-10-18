b7e240798945684001d621292e9638085a7350c1
Mudit Porwal <muditporwal@users.noreply.github.com>
2023-10-18 02:44:09 +0000

    Updated path for settings.json

--- setup/tools/code-setup.sh
+++ setup/tools/code-setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 current_dir=$(pwd)
 mkdir -p "$HOME/.config/Code - OSS"
-ln -sf "$current_dir/tools/conf/code-settings.json" "$HOME/.config/Code - OSS/settings.json"
+ln -sf "$current_dir/tools/conf/code-settings.json" "$HOME/.config/Code - OSS/User/settings.json"
